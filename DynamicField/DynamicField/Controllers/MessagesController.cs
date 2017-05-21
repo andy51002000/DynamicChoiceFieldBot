7d36f305df006df1a5650fb4e917fbdcea62db40
andy51002000 <andy51002000@gmail.com>
2017-05-21 01:33:22 -0700

    first release

---
+++ DynamicField/DynamicField/Controllers/MessagesController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+
+namespace DynamicField
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                await Conversation.SendAsync(activity, MakeRootDialog);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        internal static IDialog<SandwichOrder> MakeRootDialog()
+        {
+            return Chain.From(() => FormDialog.FromForm(SandwichOrder.BuildForm))
+                .Do(async (context, order) =>
+                {
+                    try
+                    {
+                        var completed = await order;
+                        // Actually process the sandwich order...
+                        await context.PostAsync("Processed your order!");
+                    }
+                    catch (FormCanceledException<SandwichOrder> e)
+                    {
+                        string reply;
+                        if (e.InnerException == null)
+                        {
+                            reply = $"You quit on {e.Last}--maybe you can finish next time!";
+                        }
+                        else
+                        {
+                            reply = "Sorry, I've had a short circuit.  Please try again.";
+                        }
+                        await context.PostAsync(reply);
+                    }
+                });
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ DynamicField/DynamicField/SandwishOrder.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.FormFlow.Advanced;
+
+namespace DynamicField
+{
+    public enum SandwichOptions
+    {
+        BLT, BlackForestHam, BuffaloChicken
+    };
+    public enum LengthOptions { SixInch, FootLong };
+
+    public enum CheeseOptions { American, MontereyCheddar, Pepperjack };
+
+    [Serializable]
+    public class SandwichOrder
+    {
+        public SandwichOptions? Sandwich;
+        public LengthOptions? Length;
+        public CheeseOptions? Cheese;
+        [Optional]
+        [Template(TemplateUsage.NoPreference, "None")]
+        public string Specials;
+
+        public static IForm<SandwichOrder> BuildForm()
+        {
+            return new FormBuilder<SandwichOrder>()
+                .Message("Welcome to the simple sandwich order bot!")
+                .AddRemainingFields()
+                .Field(new FieldReflector<SandwichOrder>(nameof(Specials))
+                    .SetType(null)
+                    .SetActive((state) => state.Length == LengthOptions.FootLong)
+                    .SetDefine(async (state, field) =>
+                    {
+                        var dic = GetSpecialFree(state.Sandwich);
+                        foreach (KeyValuePair<string, string[]> item in dic)
+                        {
+                            field
+                                .AddDescription(item.Key, item.Value[0])
+                                .AddTerms(item.Key, item.Value);
+                        }
+
+                        return true;
+                    }))
+
+                .Build();
+        }
+
+        private static Dictionary<string, string[]> GetSpecialFree(SandwichOptions? sandwichOptions)
+        {
+            Dictionary<string, string[]> dic = new Dictionary<string, string[]>();
+            switch (sandwichOptions)
+            {
+                case SandwichOptions.BLT:
+                    dic.Add("cookie", new string[] {"Free cookie", "cookie"});
+                    dic.Add("drink", new string[] {"Free drink", "drink"});
+                    break;
+                case SandwichOptions.BlackForestHam:
+                    dic.Add("cookie", new string[] {"Free cookie", "cookie"});
+                    dic.Add("salard", new string[] {"Free salard", "salard"});
+                    break;
+                case SandwichOptions.BuffaloChicken:
+                    dic.Add("drink", new string[] {"Free drink", "drink"});
+                    dic.Add("salard", new string[] {"Free salard", "salard"});
+                    break;
+            }
+            return dic;
+        }
+
+    };
+
+}
